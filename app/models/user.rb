11ee0a1809b5a77865bd51cff4929fcde5923871
root <root@kevin-Satellite-T215D.(none)>
2013-06-05 10:02:54 +0700

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,13 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   before_save { email.downcase! }
+  before_save :create_remember_token
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 
   validates :name, presence: true, length: { maximum: 50 }
 
